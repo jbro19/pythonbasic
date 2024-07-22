0893de64964efa7f37d78ae5e3cfe3a8dd8c2daa
brdho <brdhong@naver.com>
2024-07-22 23:12:35 +0900

    ep06 commit

---
+++ pythonbasic/ep06_whliemoon.py
@@ -0,0 +1,32 @@
+
+# range 함수 for문
+# for num in range(1,101):
+#     print(num)
+#
+# print("end")
+
+
+# while함수 조건문이 참일 경우 계속해서 하위 코드 무한반복
+# 만일 조건문이 거짓일 경우 while문 탈출
+
+# while 조건문:
+#    코드
+#    코드
+
+
+# num = 1
+# while num <= 100:
+#    print(num)
+#    num = num + 1
+#
+# print("end")
+
+num = 1
+while True:
+    print(num)
+    num = num + 1
+    if num >= 101:
+        break
+
+
+print("end")
