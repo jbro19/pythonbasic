06c34e41bc38fcb637bd6e4a8dcd05b8077a9263
brdho <brdhong@naver.com>
2024-07-22 22:51:46 +0900

    ep04 commit

---
+++ pythonbasic/ep04_datatype_structure.py
@@ -0,0 +1,50 @@
+
+
+# 정수
+age = 20
+print(age)
+print(type(age))
+
+# 실수
+weight = 80.5
+print(weight)
+print(type(weight))
+
+# 문자열
+
+greeting = "hlleo world!"
+print(greeting)
+print(type(greeting))
+
+# 블리언
+is_adult = True
+print(is_adult)
+print(type(is_adult))
+
+# ------------------------
+# 파이썬의 자료구조
+
+# 리스트(list
+fruits = [1,2,"딸기"]
+print(fruits)
+print(type(fruits))
+
+# 튜플
+location = ("37.123","126.957")
+print(location)
+print(type(location))
+
+# 딕셔너리
+student_scores = {'철수':90,'영희':85, '민수':95}
+print(student_scores)
+print(type(student_scores))
+
+# 세트
+colors = {'red','red','blue','green','blue','yellow','black'}
+print(colors)
+print(type(colors))
+
+# 중괄호는 중복을 허용하지 않음
+# print는 값 출력, type이 있을경우 해당 값의 타입을 알려줌
+
+
